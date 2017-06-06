59873f5c16834c5e5396722f1fe691dc5d531c0a
tholland15 <treyholland89@gmail.com>
2017-06-05 20:00:16 -0400

    Added deadParentsMine(), added countFiles(), added credential saving, added pickle support for large file lists on low memory/32-bit systems

--- cleaner.py
+++ cleaner.py
@@ -2,13 +2,22 @@
 
 import httplib2
 from pprint import pprint
+import sys
 
 import apiclient.discovery
 from apiclient import errors
-from oauth2client.client import OAuth2WebServerFlow
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+import os
+import cPickle as pickle
 
-from config import *
 
+# If modifying these scopes, delete your previously saved credentials
+# at ./.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'GDriveOrphans'
 
 class DriveCleaner():
     def __init__(self, drive_service):
@@ -30,9 +39,15 @@ class DriveCleaner():
         self.files = []
         self.requests = 0
         self.total = 0
+        
+        try:
+            os.remove('fileList.pkl')
+        except OSError:
+            pass
+        
         while keep_going:
             self.requests += 1
-            print 'Making request ' + str(self.requests)
+            print 'Making request ' + str(self.requests) + ' total files ' + str(self.total)
             f = self.drive.files().list(maxResults=1000, pageToken=next_token).execute()
             try:
                 next_token = f['nextPageToken']
@@ -40,21 +55,59 @@ class DriveCleaner():
                 next_token = None
                 keep_going = False
             self.total += len(f['items'])
-            self.files.extend(f['items'])
+            pf = open('fileList.pkl', 'ab')
+            pickler = pickle.Pickler(pf)
+            pickler.dump(f['items'])
+            pf.close()
         print str(self)
 
     def noParents(self):
-        return [x for x in self.files if len(x['parents']) == 0];
+        no_parents = []
+        while(1):
+            pf = open('fileList.pkl', 'rb')
+            unpickler = pickle.Unpickler(pf)
+            files = unpickler.load()
+            for file in files: 
+               if len(file['parents']) == 0]:
+                   no_parents.extend(file)
+        pf.close()                   
+        return no_parents
 
     def noParentsMine(self):
         return [x for x in self.noParents() if
                 len(x['owners']) == 1 and x['owners'][0]['isAuthenticatedUser']]
+                
+    def deadParentsMine (self):
+        # look for files whose parents return a 404
+        dead_parents = []
+        while(1):
+            pf = open('fileList.pkl', 'rb')
+            unpickler = pickle.Unpickler(pf)
+            files = unpickler.load()
+            for file in files: 
+                if len(file['owners']) == 1 and file['owners'][0]['isAuthenticatedUser']:
+                    dead_parent_count = 0
+                    for parent in file['parents']:
+                        try:
+                            self.drive.files().get(fileId=parent['id']).execute()
+                        except errors.HttpError, error:
+                            if error.resp.status == 404:
+                                if file['labels'] and file['labels']['trashed']:
+                                    pass
+                                else:
+                                    dead_parent_count += 1
+                    # if the parent count matches dead_parent_count, all parents are dead
+                    if len(file['parents']) == dead_parent_count:
+                        dead_parents.extend(file)
+                        print 'File %s has dead parents' % file['title']
+            pf.close()
+        return dead_parents             
 
     def trashItems(self, reload_files=False):
         if len(self.files) == 0 or reload_files:
             self.updateFiles()
-        # find items with no parents owned by me
-        for item in self.noParents:
+        # find items we want to trash
+        for item in self.noParentsMine() + self.deadParentsMine():
             # ... check to see if already trashed
             if item['labels'] and item['labels']['trashed']:
                 # print '\tAlready in trash: ' + item['title'] + ' - ' + item['id']
@@ -84,8 +137,8 @@ class DriveCleaner():
         """
         if len(self.files) == 0 or reload_files:
             self.updateFiles()
-        # find items with no parents owned by me
-        for item in self.noParentsMine():
+        #find items we want to move
+        for item in self.noParentsMine() + self.deadParentsMine():
             # ... check to see if already trashed
             if item['labels'] and item['labels']['trashed']:
                 # print '\tAlready in trash: ' + item['title'] + ' - ' + item['id']
@@ -101,15 +154,63 @@ class DriveCleaner():
                     self.errors += 1
         self.updateFiles()
         print str(self)
+        
+    def countFiles(self, folder):
+        keep_going = True
+        next_token = None
 
+        self.files = []
+        self.requests = 0
+        self.total = 0
+        while keep_going:
+            self.requests += 1
+            print 'Making request ' + str(self.requests) + ' total files so far ' + str(self.total)
+            f = children = self.drive.children().list(folderId=folder, maxResults=1000, pageToken=next_token).execute()
+            try:
+                next_token = f['nextPageToken']
+            except KeyError:
+                next_token = None
+                keep_going = False
+            except:
+                print "Unexpected error:", sys.exc_info()[0]
+            self.total += len(f['items'])
+            print 'Total Files: ' + str(self.total)
+
+def get_credentials():
+    try:
+        import argparse
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    except ImportError:
+        flags = None
+
+    home_dir = None
+    path = os.path.realpath(sys.argv[0])
+    if os.path.isdir(path):
+        home_dir = path
+    else:
+        home_dir = os.path.dirname(path)
+    #print 'dir: ' + str(home_dir)
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'GdriveOrphans-Creds.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
 
 def build_connection():
-    # Run through the OAuth flow and retrieve credentials
-    flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-    authorize_url = flow.step1_get_authorize_url()
-    print 'Go to the following link in your browser: ' + authorize_url
-    code = raw_input('Enter verification code: ').strip()
-    credentials = flow.step2_exchange(code)
+    
+    credentials = get_credentials()
 
     # Create an httplib2.Http object and authorize it with our credentials
     http = httplib2.Http()
@@ -118,5 +219,5 @@ def build_connection():
 
     # Create drivecleaner and run it
     dc = DriveCleaner(drive_service)
-    dc.updateFiles()
+    print 'Connection Successful'
     return dc
